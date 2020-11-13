4d5797f29ef85bb309d96ca3b76254f3179a83b5
Hayk Abrahamyan <abrahamyan.hayk.03@gmail.com>
2020-11-13 20:25:16 +0400

    Fix crash

--- src/layouts/Standard/LayoutStandard.styles.js
+++ src/layouts/Standard/LayoutStandard.styles.js
@@ -26,7 +26,7 @@ export const LayoutStandard = styled.div`
     width: 100%;
     height: 100%;
     overflow-y: scroll;
-    margin-bottom: 80px;
+    margin-bottom: ${p => p.isAuth ? '80px' : '0px'};
 
     scrollbar-color: #5E36FF transparent;
     scrollbar-width: thin;
