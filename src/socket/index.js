93b91e7d01b31297b52195558b4d29683d4ca135
Hayk Abrahamyan <abrahamyan.hayk.03@gmail.com>
2020-11-21 07:40:44 +0400

    Setup sockets

---
+++ src/socket/index.js
@@ -0,0 +1,22 @@
+import openSocket from 'socket.io-client'
+
+
+const SOCKET_ENDPOINT = 'http://localhost:5050'
+
+let socket = null
+
+export const initSocket = (userId) => {
+  socket = openSocket(SOCKET_ENDPOINT, { query: `userId=${userId}` })
+}
+
+export const subscribeSocket = (type, cb) => {
+  socket.on(type, props => cb(props))
+}
+
+export const sendMessage = (type, message) => {
+  socket.emit(type, message)
+}
+
+export const closeSocket = (userId) => {
+  socket.disconnect()
+}
